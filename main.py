88dabad9df2e04ccba33665461291a5cc8833e99
qasimmizbah1 <qasim@techbeeps.co.in>
2025-09-30 11:22:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
 
 @app.get("/")
+async def head_root():
+    return JSONResponse(status_code=200)
 async def read_root():
     return {"message": "Welcome to the Auto Parts Marketplace API"} 
     
