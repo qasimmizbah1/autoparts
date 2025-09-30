c688b616346a5f9684a3beca7360ca8a60aa7c96
qasimmizbah1 <qasim@techbeeps.co.in>
2025-09-30 11:32:49 +0530

    Update main files according to render with port

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+import os
+import uvicorn
 
 app = FastAPI()
 
@@ -8,3 +10,9 @@ app = FastAPI()
 @app.head("/")
 async def root():
     return {"message": "Welcome to the Auto Parts Marketplace API"}
+
+
+# Run with Render's assigned port
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # default 8000 for local testing
+    uvicorn.run(app, host="0.0.0.0", port=port)
